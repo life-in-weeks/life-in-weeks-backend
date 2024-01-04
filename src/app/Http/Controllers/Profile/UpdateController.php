07e9028fe9e6811384a5dd267f01c756f8bc22a2
andrey <andrey.develop.a@gmail.com>
2024-01-05 03:51:56 +0700

    feat: create update controller

--- src/app/Http/Controllers/Profile/UpdateController.php
+++ src/app/Http/Controllers/Profile/UpdateController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Profile\UpdateRequest;
+use App\Http\Resources\Profile\ProfileResource;
+use App\Models\Profile;
 use App\Models\User;
 
 class UpdateController extends Controller
 {
     public function __invoke(UpdateRequest $request)
     {
-        $user = auth()->user()->profile;
+        $profile = auth()->user()->profile;
         $data = $request->validated();
-        $user->update($data);
-        dd($user);
+        $profile->update($data);
+        return $profile instanceof Profile
+            ? new ProfileResource($profile)
+            : $profile;
     }
 }
