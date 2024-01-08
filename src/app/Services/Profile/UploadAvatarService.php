fcfb5f2ce1ca59a2f40d6e62fcf781bea7a5038e
andrey <andrey.develop.a@gmail.com>
2024-01-09 04:22:54 +0700

    refactor: Implemented image transformation to WebP format

--- src/app/Services/Profile/UploadAvatarService.php
+++ src/app/Services/Profile/UploadAvatarService.php
@@ -3,23 +3,38 @@
 namespace App\Services\Profile;
 
 use App\Models\Image;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
+
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Imagick\Driver;
 
 class UploadAvatarService
 {
     public function __invoke($data)
     {
+        $manager = new ImageManager(new Driver());
+
         try {
             $profile = auth()->user()->profile;
             $avatar = $data["images"][0];
             $path = $avatar->store("avatars", "public");
+
+            $image = $manager->read("storage/" . $path);
+            $encoded = $image->toWebp(60);
+            Storage::put("public/avatars/{$avatar->hashName()}.webp", $encoded);
+
+            Storage::disk("public")->delete($path);
+            $webpPath = "avatars/{$avatar->hashName()}.webp";
+            $webpUrl = Storage::url($webpPath);
+
             if ($profile->avatar) {
                 $profile->avatar->update([
-                    "url" => Storage::url($path),
+                    "url" => $webpUrl,
                 ]);
             } else {
-                $image = new Image(["url" => Storage::url($path)]);
+                $image = new Image(["url" => $webpUrl]);
                 $profile->image()->save($image);
             }
             return $profile->avatar;
